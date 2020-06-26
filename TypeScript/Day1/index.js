213ba86ff0ef2077e9540b51d11c8fd7ca8a476c
baohungdev <baohungdev@icloud.com>
2020-06-26 23:16:57 +0700

    Lab 2,3

--- TypeScript/Day1/index.js
+++ TypeScript/Day1/index.js
@@ -1,3 +1,10 @@
+//LAB 1
+// Create 5 variables which use
+// 5 types ( string, number
+// Boolean, array, object )
+// - Try to make a type’s error
+// --------------------------------------------- //
+// Declares
 var firstName = "Duong Bao Hung";
 var numberOfCartItems = 5;
 var toogleHidden = true;
@@ -8,3 +15,48 @@ User = {
     id: 100,
     fullName: "John Smith"
 };
+var productItem = /** @class */ (function () {
+    function productItem() {
+        var _this = this;
+        this.idCode = 0;
+        this.productName = "";
+        this.stores = [
+            {
+                storeName: "",
+                regularPrice: 0
+            },
+            {
+                storeName: "",
+                regularPrice: 0
+            },
+            {
+                storeName: "",
+                regularPrice: 0
+            },
+        ];
+        this.checkPrice = function () {
+            var prices = _this.stores.map(function (store) { return store.regularPrice; });
+            return Math.min.apply(Math, prices);
+        };
+    }
+    return productItem;
+}());
+var prM = new productItem();
+prM.idCode = 18520061;
+prM.productName = "IP11PROMAX";
+prM.stores[0].storeName = "Di Dong Viet";
+prM.stores[0].regularPrice = 29990000;
+prM.stores[1].storeName = "CellPhones";
+prM.stores[1].regularPrice = 30000000;
+prM.stores[2].storeName = "TGDD";
+prM.stores[2].regularPrice = 31000000;
+//--------------------------------------------- //
+//LAB 3
+// Create a function which uses
+// interface
+// - Try to make a type’s error
+function takeLatestPrice(productItem) {
+    return productItem.checkPrice();
+}
+var latestPrice = takeLatestPrice(prM);
+console.log(latestPrice);
--- TypeScript/Day1/index.ts
+++ TypeScript/Day1/index.ts
@@ -1,3 +1,10 @@
+//LAB 1
+// Create 5 variables which use
+// 5 types ( string, number
+// Boolean, array, object )
+// - Try to make a type’s error
+// --------------------------------------------- //
+// Declares
 const firstName: string = "Duong Bao Hung";
 let numberOfCartItems: number = 5;
 let toogleHidden: boolean = true;
@@ -11,3 +18,63 @@ User = {
   id: 100,
   fullName: "John Smith",
 };
+// Errors
+// let Error1: string = 1;
+// let Users: number[] = ["1", "3"];
+// --------------------------------------------- //
+//LAB 2
+// - Create a class which uses
+// interface
+// - Try to make a type’s error
+interface IproductItem {
+  idCode: number;
+  productName: string;
+  stores: object[];
+  checkPrice: () => number;
+  regularPrice?: number;
+}
+class productItem implements IproductItem {
+  idCode = 0;
+  productName = "";
+  stores = [
+    {
+      storeName: "",
+      regularPrice: 0,
+    },
+    {
+      storeName: "",
+      regularPrice: 0,
+    },
+    {
+      storeName: "",
+      regularPrice: 0,
+    },
+  ];
+  checkPrice = () => {
+    const prices: number[] = this.stores.map((store) => store.regularPrice);
+    return Math.min(...prices);
+  };
+}
+const prM = new productItem();
+prM.idCode = 18520061;
+prM.productName = "IP11PROMAX";
+prM.stores[0].storeName = "Di Dong Viet";
+prM.stores[0].regularPrice = 29990000;
+prM.stores[1].storeName = "CellPhones";
+prM.stores[1].regularPrice = 30000000;
+prM.stores[2].storeName = "TGDD";
+prM.stores[2].regularPrice = 31000000;
+
+//--------------------------------------------- //
+//LAB 3
+// Create a function which uses
+// interface
+// - Try to make a type’s error
+function takeLatestPrice(productItem: IproductItem): number {
+  return productItem.checkPrice();
+}
+
+const latestPrice: number = takeLatestPrice(prM);
+
+console.log(latestPrice);
+//--------------------------------------------- //
